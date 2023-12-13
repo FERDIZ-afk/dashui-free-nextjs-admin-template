2ba3d8562e3984694e9d01a9272840ffb436842c
FERDIZ-afk <ferdizakyla@gmail.com>
2023-12-13 20:04:12 +0700

    千夜未来

--- app/api/hello.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json({ message: 'Hello, this is a simple Next.js API!' });
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
