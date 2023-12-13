190b2893f338ff9a883b9447de019e13a7fef98a
FERDIZ-afk <ferdizakyla@gmail.com>
2023-12-13 19:31:29 +0700

    千夜未来

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pages: {
+    items: ['pages/**/*.{js,jsx,ts,tsx}', 'app/**/*.{js,jsx,ts,tsx}'],
+    paths: [],
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
